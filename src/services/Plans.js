3ae5e9defd3bdd94e9693505df2a4ef4b65f6030
Wellington Brindo da Cruz <260479861+WellBrindo@users.noreply.github.com>
2026-02-16 23:52:38 -0300

    Create Plans.js

---
+++ src/services/Plans.js
@@ -0,0 +1,4 @@
+// src/services/Plans.js
+// Compatibilidade de case-sensitive (Linux/Render).
+// Se algum arquivo importar './Plans.js', isso reexporta o módulo correto.
+export * from "./plans.js";
